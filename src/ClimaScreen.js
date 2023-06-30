aee92eebcace13d527a9d3bc8a17aff23409f4af
chenaoh <moralesgreydi16@gmail.com>
2023-06-30 10:55:21 -0600

    Estilos agregados

--- src/ClimaScreen.js
+++ src/ClimaScreen.js
@@ -1,17 +1,17 @@
 import React, { useEffect, useState} from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import axios from 'axios';
 
 
+const countries = ['Belize', 'Costa Rica', 'El Salvador'];
+const ClimaScreen = () => {
 const [climaData, setClimaData] = useState(null);
-
-const countries = ['Argentina', 'Brazil', 'Dominican Republic'];
 
 useEffect(() => {
     const fetchClimaData = async () => {
         try {
             const responsePromises = countries.map(country => 
-                axios.get('https://api.weatherapi.com/v1/current.json?key=0f772b67e5fc483d820171247232806&q=${contry}&lang=es')
+                axios.get(`https://api.weatherapi.com/v1/current.json?key=0f772b67e5fc483d820171247232806&q=${country}&lang=es`)
 );
 const responses = await promise.all(responsePromises);
         const climaDataArray = responses.map(response => response.data);
@@ -26,19 +26,22 @@ const responses = await promise.all(responsePromises);
 
 if (!climaData) {
     return (
-        <View>
-        <Text>Cargando ...  </Text>
+        <View style={styles.container}>
+        <Text style={styles.loadingText}>  Cargando ...  </Text>
         </View>
     );
- 
+   }
 
  return (
-    <View>
+    <View style={styles.container}>
        {climaData.map((clima, index) => (
-        <View key={index}>
-           <Text>Ubicación: {clima.location.name},  {clima.location.region},  {clima.location.country}</Text>   
+        <View key={index} style={styles.climaContainer}>
+
+           <Text style={styles.text}>Ubicación: {clima.location.name},  {clima.location.region},  {clima.location.country}</Text>   
            <Text>Temperatura: {clima.current.temp_c}ºC</Text>
            <Text>Condición: {clima.current.condition.text}</Text>
+           <Text style={styles.text}>Viento: {clima.current.wind_kph} kph</Text>
+          <Text style={styles.text}>Humedad: {clima.current.humidity}%</Text>
            <Text>---------------------------------</Text>
             </View>
        ))}
@@ -46,6 +49,26 @@ if (!climaData) {
  );
 };
 
-export default ClimaScreen;
+const styles = StyleSheet.create({
+   container: {
+     flex: 1,
+     padding: 10,
+     backgroundColor: '#B0FFF4FF',
+   },
+   loadingText: {
+     fontSize: 18,
+     textAlign: 'center',
+   },
+   climaContainer: {
+     marginBottom: 20,
+   },
+   text: {
+     fontSize: 16,
+   },
+   separator: {
+     marginTop: 10,
+     textAlign: 'center',
+   },
+ });
 
-
+ export default ClimaScreen;
